4933fdf86cf68fc34e960fec20a8b418b16d4bd1
goynhe <cysylltiad_camahort@simplelogin.com>
2022-11-22 16:09:02 +0800

    20301015experiment4

---
+++ os/target/release/build/riscv-07c8d5a6a5486a5a/build_script_build-07c8d5a6a5486a5a.d
@@ -0,0 +1,5 @@
+/mnt/os/target/release/build/riscv-07c8d5a6a5486a5a/build_script_build-07c8d5a6a5486a5a: /root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs
+
+/mnt/os/target/release/build/riscv-07c8d5a6a5486a5a/build_script_build-07c8d5a6a5486a5a.d: /root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs
+
+/root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs:
---
+++ os/target/release/deps/riscv_target-f4221ac4f5515fa4.d
@@ -0,0 +1,7 @@
+/mnt/os/target/release/deps/riscv_target-f4221ac4f5515fa4.rmeta: /root/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/riscv-target-0.1.2/src/lib.rs
+
+/mnt/os/target/release/deps/libriscv_target-f4221ac4f5515fa4.rlib: /root/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/riscv-target-0.1.2/src/lib.rs
+
+/mnt/os/target/release/deps/riscv_target-f4221ac4f5515fa4.d: /root/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/riscv-target-0.1.2/src/lib.rs
+
+/root/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/riscv-target-0.1.2/src/lib.rs:
