8535de0d053dbacb8083b000d0844ba2dc4f4779
goynhe <cysylltiad_camahort@simplelogin.com>
2022-12-06 18:25:39 +0800

    20301015experiment6

---
+++ os/target/debug/build/riscv-46fb86ec1f1b5f45/build_script_build-46fb86ec1f1b5f45.d
@@ -0,0 +1,5 @@
+/mnt/os/target/debug/build/riscv-46fb86ec1f1b5f45/build_script_build-46fb86ec1f1b5f45: /root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs
+
+/mnt/os/target/debug/build/riscv-46fb86ec1f1b5f45/build_script_build-46fb86ec1f1b5f45.d: /root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs
+
+/root/.cargo/git/checkouts/riscv-ab2abd16c438337b/11d43cf/build.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/debug/deps/01power_5-15ed1ae9a29d1938.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/debug/deps/01power_5-15ed1ae9a29d1938: src/bin/01power_5.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/debug/deps/01power_5-15ed1ae9a29d1938.d: src/bin/01power_5.rs
+
+src/bin/01power_5.rs:
